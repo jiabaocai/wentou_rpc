aaac5c5fe17d6bd442ffd559f3ac33e508817e6e
caijiabao <caijiabao@sinafenqi.com>
2018-11-19 14:13:13 +0800

    修改结构

--- bwt-eureka-server/src/main/java/com/bwtcloud/BwtEurekaServerApplication.java
+++ bwt-eureka-server/src/main/java/com/bwtcloud/BwtEurekaServerApplication.java
@@ -2,13 +2,15 @@ package com.bwtcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class BwtEurekaServerApplication {
+public class BwtEurekaServerApplication{
 
-	public static void main(String[] args) {
-		SpringApplication.run(BwtEurekaServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BwtEurekaServerApplication.class, args);
+    }
 }
